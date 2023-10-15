df649d3ae8d5f268a73a10fc4f000b294e8c52e9
Jason Chan <shunsei.chan@gmail.com>
2023-10-14 19:28:13 -0700

    Player Perfect now

--- Stealth Platformer/Assets/Scripts/Player.cs
+++ Stealth Platformer/Assets/Scripts/Player.cs
@@ -20,62 +20,80 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask enemyLayer;
 
     [SerializeField] Transform Ground_Check;
-    private Rigidbody2D rb;
     [SerializeField] private CapsuleCollider2D colider;
     [SerializeField] private Animator slashingAnimator;
 
-    private bool facingRight = true;
+    private bool jumpPressed;
+    private bool downPressed;
+    private bool slashPressed;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
-    {
-        grounded = Physics2D.OverlapCircle(Ground_Check.position, check_radius, ground_layer);
-        rb.velocity = new Vector2(horInput * speed, rb.velocity.y);
-    }
-
     void Update()
     {
         horInput = Input.GetAxis("Horizontal");
-
-        Vector2 inFrontOfPlayer = new Vector2(transform.position.x + (1 * Mathf.Sign(rb.velocity.x)), transform.position.y);
-        enemiesInFront = Physics2D.OverlapBoxAll(inFrontOfPlayer, new Vector2(2, 1), 0, enemyLayer);
-
-        if (Input.GetButtonDown("Up") && grounded && Mathf.Abs(rb.velocity.y) < maximumYVelocity)
+        if (horInput != 0)
         {
-            rb.velocity += Vector2.up * jump_force;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * Mathf.Sign(horInput), transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetButtonDown("Up"))
+        {
+            jumpPressed = true;
         }
 
         if (Input.GetButtonDown("Down"))
         {
-            RaycastHit2D hit = Physics2D.CircleCast(Ground_Check.position, colider.size.x * 0.5f, Vector2.down, platformDistance, ground_layer);
-            if (hit.point != Vector2.zero)
-            {
-                if (hit.collider.GetComponent<PlatformEffector2D>() != null)
-                    StartCoroutine(DeactivatePlatform(hit.collider));
-            }
-
+            downPressed = true;
         }
+
         //stabbing in the back
         if (Input.GetKeyDown(KeyCode.Space))
         {
             slashingAnimator.SetTrigger("slash");
-            foreach (Collider2D enemy in enemiesInFront)
-            {
-                if (enemy.transform.localEulerAngles.y < 5 && facingRight == true)//you are both looking right
-                    Destroy(enemy.gameObject);
-                else if (enemy.transform.localEulerAngles.y > 170 && facingRight == false)//you are both looking left
-                    Destroy(enemy.gameObject);
-            }
+            slashPressed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(Ground_Check.position, check_radius, ground_layer);
+
+        Vector2 inFrontOfPlayer = new Vector2(transform.position.x + (1 * Mathf.Sign(rb.velocity.x)), transform.position.y);
+        enemiesInFront = Physics2D.OverlapBoxAll(inFrontOfPlayer, new Vector2(2, 1), 0, enemyLayer);
+
+        jumpPressed = jumpPressed && grounded;
+        if (jumpPressed)
+        {
+            rb.velocity += Vector2.up * jump_force;
+            jumpPressed = false;
         }
 
-        if (horInput > 0 && facingRight == false)// going right but facing left
-            Flip();
-        else if (horInput < 0 && facingRight == true) // going left but looking right
-            Flip();
+        if (slashPressed)
+        {
+            foreach (Collider2D enemy in enemiesInFront)
+            {
+                Destroy(enemy.gameObject);
+            }
+            slashPressed = false;
+        }
+
+        if (downPressed)
+        {
+            RaycastHit2D hit = Physics2D.CircleCast(Ground_Check.position, colider.size.x * 0.5f, Vector2.down, platformDistance, ground_layer);
+            if (hit)
+            {
+                if (hit.collider.GetComponent<PlatformEffector2D>() != null)
+                    StartCoroutine(DeactivatePlatform(hit.collider));
+            }
+            downPressed = false;
+        }
+
+        rb.velocity = new Vector2(horInput * speed, rb.velocity.y);
     }
 
     IEnumerator DeactivatePlatform(Collider2D platformColider)
@@ -84,19 +102,14 @@ public class Player : MonoBehaviour
         rb.AddForce(Vector2.down * 3f);
         while (colider.size.y + transform.position.y > platformColider.transform.position.y)//still above
         {
-            yield return new WaitForSeconds(0.05f);// wait a bit
+            yield return null;
         }
         platformColider.enabled = true;
     }
 
-    private void Flip()
-    {
-        transform.Rotate(Vector2.up * 180);
-        facingRight = !facingRight;
-    }
-
     private void OnDrawGizmos()
     {
+        if (rb != null) Gizmos.DrawWireCube(new Vector2(transform.position.x + (1 * Mathf.Sign(rb.velocity.x)), transform.position.y), new Vector2(2, 1));
         Gizmos.DrawWireSphere(Ground_Check.position, check_radius);
         Gizmos.DrawLine(Ground_Check.position, Ground_Check.position + Vector3.down * platformDistance);
         Gizmos.DrawWireSphere(Ground_Check.position + Vector3.down * platformDistance, colider.size.x * 0.5f);
